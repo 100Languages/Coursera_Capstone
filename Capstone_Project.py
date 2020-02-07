adb334f599d71186f3abbf822c243b5e37e81ce4
Vera <vera@Veras-MacBook-Pro.local>
2020-02-07 13:14:52 -0500

    adds a python script that prints a message greeting Capstone Course

---
+++ Capstone_Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This notebook is the capstone project for the IBM Data Science Professional Certificate via Coursera
+
+# In[11]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[12]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
